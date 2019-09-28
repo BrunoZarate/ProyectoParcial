332987b6067ce939465829de1b840d2b14630b91
Bruno Ivan Zárate Fernández <brunoivanzaratefernandez@gmail.com>
2019-09-27 20:16:26 -0400

    Segundo Commit (Creacion modelo Estudiante)

---
+++ WebEvaluation/Models/Estudiante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebEvaluation.Models
+{
+    public enum Genre
+    {
+        Male,
+        Female,
+    }
+    public class Estudiante
+    {
+        [Key]
+        public int EstudianteId { get; set; }
+        [Required(ErrorMessage="Please Enter a Name")]
+        public string Name { get; set; }
+        [Required]
+        public Genre Genero { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime? BirthDate { get; set; }
+    }
+}
